3a886a23eb7409d3b38ed5d3839aba9edfe4194c
Michael Zatlin <michael.zatlin@yahoo.com>
2018-04-26 17:21:21 -0700

    Implemented basic player movement, integrated Probuilder Core

---
+++ TopDown3D/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    public float movementspeed = 3f;
+
+    private Vector3 move;
+    private Rigidbody rb;
+
+
+    // Use this for initialization
+    void Start()
+    {
+   
+       rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        move = new Vector3(-Input.GetAxis("Horizontal"), 0.0f, -Input.GetAxis("Vertical"));
+     //(move * Time.deltaTime * movementspeed);
+
+       rb.velocity= move * movementspeed;
+    }
+}
